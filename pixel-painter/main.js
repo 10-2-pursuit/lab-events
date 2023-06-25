23e8a8d1e50dfb64129bde779a6d9f87a9fd1728
Tonesha Rose <131703989+tonesharose31@users.noreply.github.com>
2023-06-25 13:02:34 -0400

    working with Aisha and Gensis

---
+++ pixel-painter/main.js
@@ -0,0 +1,42 @@
+
+const palette = document.getElementById("palette");
+const currentColor = document.getElementById("currentColor");
+
+let canvas = document.querySelector("#canvas");
+
+const newDiv = document.createElement("div");
+const colors = document.querySelectorAll(".color"); 
+
+const cells = document.querySelectorAll(".cell");
+
+
+for(let color of colors){
+    color.addEventListener("click",(event)=> {
+        currentColor.style.background = event.target.style.background;
+       
+    });
+
+}
+
+for (let cell of cells){
+    cell.addEventListener("click",(event) => {
+        cell.style.transform = "change color"
+    });
+}
+
+
+const newButton =document.createElement("button")
+newButton.textContent = "New Color"
+
+newDiv.append(newButton); 
+palette.append(newDiv);
+
+
+newButton.addEventListener("click", ()=> {
+    for(let cell of cells){
+        cell.style.background = "white";
+console.log(newButton.addEventListener)
+    }
+    //currentColor.style.background = "white";
+})
+ 
