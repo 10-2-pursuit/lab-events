aebcc29438394e142337c5adcc999a3d7ebc1988
Shanice Griffin <shanicegriffin@Shanices-MacBook-Air.local>
2023-06-25 17:29:02 -0400

    something done

--- tic-tac-toe/main.js
+++ tic-tac-toe/main.js
@@ -7,7 +7,56 @@ function makeBoard() {
       cell.classList.add('empty', 'square');
       container.appendChild(cell);
     }
-  }
-  
+}
 makeBoard()
 
+
+let currentPlayer = 'X';
+
+function makeMove(event) {
+  const square = event.target;
+
+  if (square.classList.contains('empty')) {
+    square.textContent = currentPlayer;
+    square.classList.remove('empty');
+
+    currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+  }
+}
+// Attach the event handler to all square elements
+const squares = document.querySelectorAll('.square');
+squares.forEach(square => {
+  square.addEventListener('click', makeMove);
+});
+
+// Reset Button
+const button = document.querySelector('button');
+button.classList.add("reset-button");
+
+function reset() {
+ makeBoard();
+}
+
+
+const resetButton = document.getElementById('.reset-button');
+resetButton.addEventListener('click', reset);
+
+// Game Over Function
+const container = document.querySelector('.tic-tac-toe');
+
+function gameOver() {
+    let game = false;
+    squares.forEach(square => {
+        if (square == "") {
+            game = true;
+        }
+    });
+    
+    const div = document.querySelector(".tic-tac-toe");
+    const newH3 = document.createElement("h3");
+    newH3.textContent = "GAME OVER!";
+    div.append(newH3);
+
+    return game;
+}
+gameOver()
